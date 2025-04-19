dfa1ccc305f6ec44e72bea0313fb7dc3593e173b
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-04-19 17:31:59 +0100

    //

--- thread/app/home/index.jsx
+++ thread/app/home/index.jsx
@@ -1,7 +1,7 @@
 import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo'
 import { Link } from 'expo-router'
 import { Text, View } from 'react-native'
-import { SignOutButton } from '@/app/components/SignOutButton'
+// import { SignOutButton } from '.'
 
 export default function Page() {
   const { user } = useUser()
@@ -10,7 +10,7 @@ export default function Page() {
     <View>
       <SignedIn>
         <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
-        <SignOutButton />
+        {/* <SignOutButton /> */}
       </SignedIn>
       <SignedOut>
         <Link href="/(auth)/sign-in">
